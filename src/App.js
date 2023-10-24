6c76d7089df55de841b4e999347ecc649944a8cd
PVSKR <skr.polu@hotmail.com>
2023-10-24 21:18:26 +0530

    3d cube is creating with the help of animations.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="wrapper">
+        <div class="box-area">
+          <div class="box box-front"></div>
+          <div class="box box-right"></div>
+          <div class="box box-back"></div>
+          <div class="box box-left"></div>
+          <div class="box box-top"></div>
+          <div class="box box-bottom"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
